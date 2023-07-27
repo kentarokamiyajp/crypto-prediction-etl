8f8e82b3c6786e4a057537c9cf47d854607d2597
kamiken <kentaro.kamiya.0425@gmail.com>
2023-07-27 14:52:04 +0900

    add hadoop fs script

--- script/hadoop_operations/copy_to_hdfs.sh
+++ script/hadoop_operations/copy_to_hdfs.sh
@@ -12,7 +12,7 @@ if [ $# != 3 ]; then
     echo "##############################################"
     echo "Argments Eroor !!!"
     echo "usage: copy_to_hdfs.sh <src_type> <src_file/dir> <dst_dir>"
-    echo "src_type: dir -> directory, fl -> file"
+    echo "src_type: -d -> directory, -f -> file"
     echo "##############################################"
     exit 1
 fi
@@ -21,31 +21,39 @@ SRC_TYEP=$1
 SRC_NAME=$2
 DEST_DIR=$3
 
-DEFAULT_ENV="${COMMON_HOME}/env_variables.sh"
-
-if [ ! -f "${DEFAULT_ENV}" ]; then
+# Source file/dir type check
+case "${SRC_TYEP}" in
+    "-f")
+    ;;
+    "-d")
+    ;;
+  *)
     echo "##############################################"
-    echo "${DEFAULT_ENV} READ Failded !!!"
+    echo "src_type '${SRC_TYEP}' is wrong !!!"
+    echo "src_type: -d -> directory, -f -> file"
+    echo "##############################################"
+    exit 1
+    ;;
+esac
+
+
+DEFAULT_CONF="default_conf.sh"
+
+if [ ! -f "${DEFAULT_CONF}" ]; then
+    echo "##############################################"
+    echo "${DEFAULT_CONF} READ Failded !!!"
+    echo "Wrong working directory or file not found !!!"
     echo "##############################################"
     exit 1
 fi
 
-. "${DEFAULT_ENV}"
+. $DEFAULT_CONF
 
 SRC_PATH="${EXT_DATASET}/${SRC_NAME}"
 
-# Source file/dir type check
-src_type_list=("dir" "fl")
-if ! [[ $(echo "${src_type_list[@]}" | fgrep -w "${SRC_TYEP}") ]]; then
-    echo "##############################################"
-    echo "src_type '${SRC_TYEP}' is wrong !!!"
-    echo "src_type: dir -> directory, fl -> file"
-    echo "##############################################"
-    exit 1
-fi
 
 # Source file existance check
-if [ "${SRC_TYEP}" = "fl" ]; then
+if [ "${SRC_TYEP}" = "-f" ]; then
     if [ ! -f "${SRC_PATH}" ]; then
         echo "##############################################"
         echo "${SRC_PATH} READ Failded !!!"
