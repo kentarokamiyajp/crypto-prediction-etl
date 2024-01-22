3acd476a31bf40f5e2ee6ed4c7f1831de454f57c
kamikennn <kentaro.kamiya.0425@gmail.com>
2024-01-22 23:25:02 +0900

    update cross use model

--- script/dbt/crypto_etl_pjr/models/example/cross_use__row_count.sql
+++ script/dbt/crypto_etl_pjr/models/example/cross_use__row_count.sql
@@ -1,13 +1,13 @@
 {{ config(materialized='table') }}
 
-select 'crypto_raw.candles_day' as source_tbl, count(*) from {{ ref('stg_crypto_raw__candles_day') }}
+select 'crypto_raw.candles_day' as source_tbl, count(*) as record_count from {{ ref('stg_crypto_raw__candles_day') }}
 union all
-select 'forex_raw.forex_rate_day' as source_tbl, count(*) from {{ ref('stg_forex_raw__forex_rate_day') }}
+select 'forex_raw.forex_rate_day' as source_tbl, count(*) as record_count from {{ ref('stg_forex_raw__forex_rate_day') }}
 union all
-select 'gas_raw.natural_gas_price_day' as source_tbl, count(*) from {{ ref('stg_gas_raw__natural_gas_price_day') }}
+select 'gas_raw.natural_gas_price_day' as source_tbl, count(*) as record_count from {{ ref('stg_gas_raw__natural_gas_price_day') }}
 union all
-select 'gold_raw.gold_price_day' as source_tbl, count(*) from {{ ref('stg_gold_raw__gold_price_day') }}
+select 'gold_raw.gold_price_day' as source_tbl, count(*) as record_count from {{ ref('stg_gold_raw__gold_price_day') }}
 union all
-select 'oil_raw.crude_oil_price_day' as source_tbl, count(*) from {{ ref('stg_oil_raw__crude_oil_price_day') }}
+select 'oil_raw.crude_oil_price_day' as source_tbl, count(*) as record_count from {{ ref('stg_oil_raw__crude_oil_price_day') }}
 union all
-select 'stock_raw.stock_index_day' as source_tbl, count(*) from {{ ref('stg_stock_raw__stock_index_day') }}
+select 'stock_raw.stock_index_day' as source_tbl, count(*) as record_count from {{ ref('stg_stock_raw__stock_index_day') }}
