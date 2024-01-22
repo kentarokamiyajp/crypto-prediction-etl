c2e23ad4b2f1355af538ce07b53b8ea5493638d9
kamikennn <kentaro.kamiya.0425@gmail.com>
2024-01-22 23:18:03 +0900

    add cross_use model

--- script/dbt/crypto_etl_pjr/dbt_run.sh
+++ script/dbt/crypto_etl_pjr/dbt_run.sh
@@ -9,19 +9,28 @@
 # Set variables
 profile_dir=/home/git/crypto_prediction_dwh/script/dbt/crypto_etl_pjr
 target="dev"
+
+
+############
+# Debug
+############
+echo "running dbt debug ..."
+dbt debug --profiles-dir ${profile_dir}
+
+
+############
+# RUN
+############
+echo "running dbt run ..."
+
+# create staging views
 profile="cross_use"
 model="staging"
-
-
-# Debug
-# echo "running dbt debug ..."
-# dbt debug --profiles-dir ${profile_dir}
-
-
-# RUN
-echo "running dbt run ..."
 dbt run --profiles-dir ${profile_dir} --target ${target} --profile ${profile} --select ${model}
 
+profile="cross_use"
+model="example"
+dbt run --profiles-dir ${profile_dir} --target ${target} --profile ${profile} --select ${model}
 
 
 # Deactivate
--- script/dbt/crypto_etl_pjr/env_conf.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-common_home="/home/git/crypto_prediction_dwh/script/common"; export common_home
